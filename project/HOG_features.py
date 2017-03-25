1fc893ec649d1e0afe876c38aea9236dcbb79301
Vegard Kamsvåg <vegardkamsvaag@gmail.com>
2017-03-25 13:01:20 +0100

    Split images into training set and test set

--- project/HOG_features.py
+++ project/HOG_features.py
@@ -26,3 +26,27 @@ ax2.imshow(hog_image_rescaled, cmap=plt.cm.gray)
 ax2.set_title('Histogram of Oriented Gradients')
 ax1.set_adjustable('box-forced')
 plt.show()
+
+
+# function for reading the images
+# arguments: path to the traffic sign data, for example './GTSRB/Training'
+# returns: list of images, list of corresponding labels
+def readTrafficSigns(rootpath):
+    '''Reads traffic sign data for German Traffic Sign Recognition Benchmark.
+
+    Arguments: path to the traffic sign data, for example './GTSRB/Training'
+    Returns:   list of images, list of corresponding labels'''
+    images = [] # images
+    labels = [] # corresponding labels
+    # loop over all 42 classes
+    for c in range(0,43):
+        prefix = rootpath + '/' + format(c, '05d') + '/' # subdirectory for class
+        gtFile = open(prefix + 'GT-'+ format(c, '05d') + '.csv') # annotations file
+        gtReader = csv.reader(gtFile, delimiter=';') # csv parser for annotations file
+        gtReader.next() # skip header
+        # loop over all images in current annotations file
+        for row in gtReader:
+            images.append(plt.imread(prefix + row[0])) # the 1th column is the filename
+            labels.append(row[7]) # the 8th column is the label
+        gtFile.close()
+    return images, labels
