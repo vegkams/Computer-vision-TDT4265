1c910dd3737f21035510eb740954564b28e786a2
vegkams <vegard.kamsvag@stud.hials.no>
2017-03-27 21:06:30 +0200

    Add methods for creating a image pyramid and sliding window

--- TrafficSignDetection/App.py
+++ TrafficSignDetection/App.py
@@ -2,9 +2,10 @@
 # -*- coding: utf-8 -*-
 
 
-
+import imutils
 import numpy as np
 import matplotlib.pyplot as plt
+import ROI_canny_hough
 
 import gtsrb
 from classifiers import MultiClassSVM
@@ -79,6 +80,25 @@ def main():
             ax[s].set_title(strategies[s])
 
         plt.show()
+
+# Create a pyramid of images of decreasing size (iterable method)
+def pyramid(image, scale = 1.5, minSize=(30, 30)):
+    yield image
+
+    while True:
+        w = int(image.shape[1]/scale)
+        image = imutils.resize(image,width=w)
+
+        if image.shape[0] < minSize[1] or image.shape[1] < minSize[0]:
+            break
+        yield image
+
+# Slide a window over the image. Returns the image along with it's x and y coordinates
+def sliding_window(image, stepSize, windowSize):
+
+    for y in range(0, image.shape[0], stepSize):
+        for x in range(0, image.shape[1], stepSize):
+            yield(x, y, image[y:y + windowSize[1], x:x + windowSize[0]])
 
 
 
