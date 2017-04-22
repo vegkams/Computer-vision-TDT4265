a0964ce4728e16567ad1037808c6f600e987eb56
vegkams <vegard.kamsvag@stud.hials.no>
2017-04-22 11:43:22 +0200

    Add example code for mser and HSV-thresholding

--- TrafficSignDetection/mser.py
+++ TrafficSignDetection/mser.py
@@ -1,31 +1,32 @@
 import cv2
 import numpy as np
 # Params for MSER
-_delta = 4
-_min_area = 50
-_max_area = 8000
-_max_variation = 0.1
-_min_diversity = 5
-_max_evolution = 1
-_area_threshold = 0.5
+_delta = 2
+_min_area = 100
+_max_area = 10000
+_max_variation = 0.25
+_min_diversity = 0.2
+_max_evolution = 150
+_area_threshold = 1.01
 _min_margin = 0.1
 _edge_blur_size = 1
 
 #Params for sorting areas
 _width_min = 14
-_width_max = 100
+_width_max = 150
 _height_min = 14
-_height_max = 110
-_hw_min = 0.8
-_hw_max = 1.2
+_height_max = 150
+_hw_min = 0.85
+_hw_max = 1.15
 
-img = cv2.imread("00001.ppm");
+img = cv2.imread("datasets/TrainIJCNN2013/00012.ppm");
 vis = img.copy()
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#mser = cv2.MSER_create()
 mser = cv2.MSER_create(_delta, _min_area, _max_area, _max_variation, _min_diversity, \
                        _max_evolution, _area_threshold, _min_margin , _edge_blur_size)
 # bbox: [min x, min y, max x-min x, max y-min y]
-regions, bbox = mser.detectRegions(gray)
+regions, bbox = mser.detectRegions(img)
 filtered_regions = list()
 filtered_bbox = list()
 i = 0
@@ -36,34 +37,35 @@ for box in bbox:
         i += 1
         pass
     else:
+        box[2] = box[0] + box[2]
+        box[3] = box[1] + box[3]
         filtered_regions.append(regions[i])
         filtered_bbox.append(box)
         i += 1
-i = 0
-print(filtered_regions[0])
-'''filtered_regions2 = list()
-filtered_bbox2 = list()
-for region in filtered_regions:
-    peri = cv2.arcLength(region, True)
-    approx = cv2.approxPolyDP(region, 0.04 * peri, True)
-    print(approx)
-    print("mellomrom \n")
-    if len(approx) == 3:
-        filtered_regions2.append(region)
-        filtered_bbox2.append(filtered_bbox[i])
-        print("Found triangle")
-    elif len(approx) == 4:
-        filtered_regions2.append(region)
-        filtered_bbox2.append(filtered_bbox[i])
-        print("Found square")
-    i += 1
-'''
 
+print(filtered_regions[0])
+
+# Duplicate each box, such that each box has at least one overlapping box
+filtered_bbox_dup = filtered_bbox.copy()
 print(len(filtered_bbox))
-print(len(filtered_regions))
+for box in filtered_bbox_dup:
+    filtered_bbox.append(box)
+
+rectList, weights = cv2.groupRectangles(filtered_bbox, 1, 0.05)
+rectList_dup = rectList.tolist()
+rectList = rectList.tolist()
+for box in rectList_dup:
+    rectList.append(box)
+
+
 hulls = [cv2.convexHull(p.reshape(-1, 1, 2)) for p in filtered_regions]
 #print(hulls)
-cv2.polylines(vis, hulls, 1, (0, 255, 0))
+#cv2.polylines(vis, hulls, 1, (0, 255, 0))
+#for box in filtered_bbox:
+#    cv2.rectangle(vis,(box[0], box[1]),(box[2], box[3]),(0, 0, 255), 1)
+
+for rectangle in rectList:
+    cv2.rectangle(vis,(rectangle[0], rectangle[1]),(rectangle[2], rectangle[3]),(255, 0, 255), 2)
 cv2.imshow('img', vis)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
